57e7e7157f88a1fcacd403e0849aed251649b601
kiwpp <kiwppgit@gmail.com>
2017-06-29 10:33:44 +0200

    NWWW upload
    uploading the internal nww site for tests

---
+++ stanton.ice.put.poznan.pl/nowak/xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>http://stanton.ice.put.poznan.pl/nowak</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://stanton.ice.put.poznan.pl/nowak/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://stanton.ice.put.poznan.pl/nowak/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://stanton.ice.put.poznan.pl/nowak/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://stanton.ice.put.poznan.pl/nowak/xmlrpc.php" />
+      <api name="Atom" blogID="" preferred="false" apiLink="http://stanton.ice.put.poznan.pl/nowak/wp-app.php/service" />
+    </apis>
+  </service>
+</rsd>
